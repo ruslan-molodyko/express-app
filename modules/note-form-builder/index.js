c7b955556beaf551217376577e27540a00d300ab
ruslan-molodyko <ruslan.molodyko@gmail.com>
2016-02-08 23:43:31 +0200

    Added test to builder #1

--- modules/note-form-builder/index.js
+++ modules/note-form-builder/index.js
@@ -15,7 +15,40 @@ var Form = ABone.create(function() {
      * @param form
      */
     this.constructor = function(form) {
-        this.form = {};
+
+        // Check argument
+        if (form == null) {
+            throw new Error('Arguments is not valid');
+        }
+
+        this.form = form;
+
+        // Properties which is not attribute of form html element
+        this.formProperties = ['name'];
+
+        // Properties which is not attribute of field html element
+        this.fieldProperties = ['label'];
+
+        this.defaultData = {
+            _form_: {
+                attr: {
+                    action: '/',
+                    method: 'POST',
+                    class: '.form-builder-'
+                }
+            },
+            field: {
+                attr: {
+                    type: 'text'
+                }
+            }
+        };
+
+        // Convert arguments to right for of data object
+        this.data = this.convert();
+
+        // Init default values of form
+        this.initDefault();
     };
 
     /**
@@ -23,14 +56,123 @@ var Form = ABone.create(function() {
      */
     this.initDefault = function() {
 
+        // Iterate all keys of data object
+        for (var key in this.data) {
+            var value = this.data[key];
+
+            // Set form data
+            if (key == '_form_') {
+
+                if (typeof value.attr['action'] === 'undefined') {
+                    value.attr['action'] = this.defaultData._form_.attr.action;
+                }
+                if (typeof value.attr['method'] === 'undefined') {
+                    value.attr['method'] = this.defaultData._form_.attr.method;
+                }
+                if (typeof value.attr['class'] === 'undefined') {
+                    value.attr['class'] = this.defaultData._form_.attr.class + this.data._form_['name'];
+                }
+
+                // Set fields data
+            } else {
+                if (typeof value.attr['type'] === 'undefined') {
+                    value.attr['type'] = this.defaultData.field.attr.type;
+                }
+                if (typeof value['name'] === 'undefined') {
+                    value.attr['name'] = key;
+                }
+                if (typeof value['label'] === 'undefined') {
+                    value['label'] = key;
+                }
+            }
+        }
     };
 
     /**
-     * Convert config to data
+     * Convert config to native data
      */
     this.convert = function() {
 
+        // Create frame
+        var form = {
+            _form_: {
+                attr: {}
+            }
+        };
+
+        // Iterate all keys of form and get properties for form
+        for (var key in this.form) {
+            var value = this.form[key];
+
+            // Skip properties for fields
+            if (key == 'fields') {
+
+                // Iterate all fields
+                for (var fieldKey in this.form.fields) {
+                    var fieldValue = this.form.fields[fieldKey];
+
+                    // Create field if not exists
+                    if (form[fieldKey] == null) {
+                        form[fieldKey] = {
+                            attr: {}
+                        };
+                    }
+
+                    // Iterate all attributes of field
+                    for (var fieldAttrKey in fieldValue) {
+                        var fieldAttrValue = fieldValue[fieldAttrKey];
+
+                        // Set attribute as field attribute or as html input
+                        if (this.fieldProperties.indexOf(fieldAttrKey) !== -1) {
+                            form[fieldKey][fieldAttrKey] = fieldAttrValue;
+                        } else {
+
+                            // If there is attr object then merge previous attributes with them
+                            if (key == 'attr') {
+
+                                form[fieldKey].attr = Object.assign(form[fieldKey].attr, fieldAttrValue);
+                            } else {
+
+                                form[fieldKey].attr[fieldAttrKey] = fieldAttrValue;
+                            }
+                        }
+                    }
+                }
+
+                // Go to next field
+                continue;
+            }
+
+            // If there is key which is not attribute of html form element then save it to form
+            if (this.formProperties.indexOf(key) !== -1) {
+
+                form._form_[key] = value;
+
+                // Or save it to attributes array
+            } else {
+                // If there is attr object then merge previous attributes with them
+                if (key == 'attr') {
+
+                    form._form_.attr = Object.assign(form._form_.attr, value);
+                } else {
+
+                    form._form_.attr[key] = value;
+                }
+            }
+        }
+
+        // Save to native data
+        return form;
     };
+
+    /**
+     * Get data from form
+     * @returns {*}
+     */
+    this.getForm = function() {
+
+        return this.data;
+    }
 });
 
 module.exports = Form;
--- modules/note-form-builder/test/form-test.js
+++ modules/note-form-builder/test/form-test.js
@@ -3,23 +3,48 @@
  */
 var path = require('path'),
     Form = require(path.join(__dirname, '..', 'index')),
-    YAML = require('yamljs'),
-    expect = require("chai").expect,
-    config = YAML.load(path.join(__dirname, '..', '..', '..', 'config/forms/security.yml'))
-    ;
+    expect = require("chai").expect;
+    config = {
+        action: '/login',
+        method: 'post',
+        name: 'login',
+        attr: {
+            class: '.my-form',
+            style: {
+                width: '250px',
+                margin: 'auto'
+            }
+        },
+        fields: {
+            email: {
+                label: 'email',
+                type: 'text',
+                name: 'email'
+            },
+            name: {
+                label: 'name',
+                type: 'text',
+                name: 'name'
+            }
+        }
+    };
 
 describe('Form#initialization', function() {
+
     it('Form class is exists', function() {
         expect(Form).to.exist;
     });
+
     it('New form return object', function() {
         expect(new Form(config)).to.exist;
     });
+
     it('Form class not working without argument in constructor', function() {
         expect(function() {
             new Form()
         }).to.throw(Error);
     });
+
     it('Correct handle of arguments', function() {
         expect(function() {
             new Form(config);
@@ -27,9 +52,88 @@ describe('Form#initialization', function() {
     });
 });
 
-describe('Form#initDefault', function() {
+describe('Form#convert', function() {
+
     var form = new Form(config);
+
     it('Data exists', function() {
         expect(form).to.ownProperty('form');
+    });
+
+    it('Return object', function() {
+        expect(form.convert()).to.be.a('object');
+    });
+
+    it('Return right value', function() {
+       expect(form.convert()).to.eql({
+            _form_: {
+                name: 'login',
+                attr: {
+                    action: '/login',
+                    method: 'post',
+                    class: '.my-form',
+                    style: {
+                        width: '250px',
+                        margin: 'auto'
+                    }
+                }
+            },
+            email: {
+                label: 'email',
+                attr: {
+                    type: 'text',
+                    name: 'email'
+                }
+            },
+            name: {
+                label: 'name',
+                attr: {
+                    type: 'text',
+                    name: 'name'
+                }
+            }
+        });
+    });
+});
+
+describe('Form#initValue', function() {
+
+    it('Data exists', function() {
+        var form = new Form(config);
+        expect(form).to.ownProperty('data');
+    });
+
+    it('Return right data', function() {
+        var form = new Form({
+            name: 'login',
+            fields: {
+                email: {}
+            }
+        });
+
+        expect(form.data).to.eql({
+            _form_: {
+                name: 'login',
+                attr: {
+                    action: '/',
+                    method: 'POST',
+                    class: '.form-builder-login'
+                }
+            },
+            email: {
+                label: 'email',
+                attr: {
+                    type: 'text',
+                    name: 'email'
+                }
+            }
+        });
+    });
+});
+
+describe('Form#getForm', function() {
+    it('Equal values', function() {
+        var form = new Form(config);
+        expect(form.getForm()).to.eql(form.data);
     });
 });
